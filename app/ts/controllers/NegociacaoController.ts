37db9e37074b0093757bd5020be9e66d47380daf
Igor Cazé Nunes <igorcaze@gmail.com>
2020-01-13 18:31:00 -0300

    Adicionado nova regra de negocio ao Controller.

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -29,6 +29,13 @@ export class NegociacaoController {
     adiciona(event: Event) {
         event.preventDefault();
 
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if(!this._ehDiaUtil(data)) {
+            this._mensagemView.update('Negociação negada, aceitamos somente negociações em dias úteis!');
+            return;
+        }
+
         const negociacao = new Negociacao(
             new Date(this._inputData.val().replace(/-/g, ',')),
             parseInt(this._inputQuantidade.val()),
@@ -40,4 +47,18 @@ export class NegociacaoController {
         this._mensagemView.update('Negociação adicionada com sucesso!');
     }
 
+    private _ehDiaUtil(data: Date) {
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+
+}
+
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
 }
